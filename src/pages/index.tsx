64da8efe4b0178229589d95cb2fa28ae9a7ed28f
Olek <aleksander.atamanczuk@gmail.com>
2021-05-21 11:28:42 +0200

    build: style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+
 import Layout from "../components/layout";
+import Header from "components/Header";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <Header />
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   );
